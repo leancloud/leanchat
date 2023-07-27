93a13fc8807dc2f433a9c7d6baf7d3404cfde4be
sdjdd <i@sdjdd.com>
2023-07-27 18:52:17 +0800

    feat: deploy to leanengine

---
+++ packages/app/scripts/generate-env.js
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+
+const prefix = 'VITE_';
+
+const envNames = ['LEANCLOUD_APP_ID', 'LEANCLOUD_APP_KEY', 'LEANCLOUD_API_SERVER'];
+
+const content = envNames
+  .map((name) => [prefix + name, process.env[name]])
+  .filter(([, value]) => !!value)
+  .map(([name, value]) => `${name}=${value}`)
+  .join('\n');
+
+fs.writeFileSync('.env.prod', content);
---
+++ server.js
@@ -0,0 +1,1 @@
+console.log('Make LeanEngine Happy ;)');
