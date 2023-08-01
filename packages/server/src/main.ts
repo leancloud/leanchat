aa020b37ea6167041d0b92e68ea76b5a68f99acf
sdjdd <i@sdjdd.com>
2023-08-01 16:13:59 +0800

    feat: redis session store

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import session from 'express-session';
+import RedisStore from 'connect-redis';
+import Redis from 'ioredis';
 import AV from 'leancloud-storage';
 import 'dotenv/config';
 
@@ -14,11 +16,17 @@ AV.init({
 });
 
 async function bootstrap() {
+  const sessionStore = new RedisStore({
+    client: new Redis(),
+    prefix: 'chat_session:',
+  });
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.disable('x-powered-by');
   app.setGlobalPrefix('api');
   app.use(
     session({
+      store: sessionStore,
       name: 'sid',
       secret: process.env.LEANCLOUD_APP_MASTER_KEY!,
       resave: false,
