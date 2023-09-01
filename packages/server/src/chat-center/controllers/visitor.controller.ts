57ace12b6591ff482e09c3392f5a5a4e044904bc
sdjdd <i@sdjdd.com>
2023-09-01 14:38:38 +0800

    fix(server): use zod validation pipe

--- packages/server/src/chat-center/controllers/visitor.controller.ts
+++ packages/server/src/chat-center/controllers/visitor.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 import { MessageService } from 'src/message';
 import { GetMessagesDto } from '../dtos/message';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('visitors')
+@UseGuards(AuthGuard)
+@UsePipes(ZodValidationPipe)
 export class VisitorController {
   constructor(private messageService: MessageService) {}
 
