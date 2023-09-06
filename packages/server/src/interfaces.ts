48b7a0023090c1d57a157be5dbd326c2fbe128eb
sdjdd <i@sdjdd.com>
2023-09-06 19:04:51 +0800

    chore(server): remove unused code

--- packages/server/src/interfaces.ts
+++ packages/server/src/interfaces.ts
@@ -7,18 +7,3 @@ export interface IMessage {
   };
   data: any;
 }
-
-export interface IConversation {
-  id: string;
-  visitorId: string;
-  operatorId?: string;
-  lastMessage?: IMessage;
-  status: string;
-  queuedAt?: Date;
-  visitorLastActivityAt?: Date;
-  evaluation?: {
-    star: number;
-    feedback: string;
-  };
-  createdAt: Date;
-}
