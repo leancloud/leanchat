e4218cc255406b99489b32a077eb7efb0ded34d5
sdjdd <i@sdjdd.com>
2023-10-24 19:20:42 +0800

    debug(server): print redis config

--- packages/server/src/config/redis.config.ts
+++ packages/server/src/config/redis.config.ts
@@ -6,7 +6,15 @@ export const redisConfig = registerAs('redis', () => {
   const env = { ...process.env };
   const { REDIS_URL_CACHE, REDIS_URL_QUEUE, REDIS_URL_MESSAGE } = env;
 
-  console.log({ REDIS_URL_CACHE, REDIS_URL_QUEUE, REDIS_URL_MESSAGE });
+  console.log(
+    'Redis config:',
+    { REDIS_URL_CACHE, REDIS_URL_QUEUE, REDIS_URL_MESSAGE },
+    {
+      cache: REDIS_URL_CACHE && expand(REDIS_URL_CACHE, env),
+      queue: REDIS_URL_QUEUE && expand(REDIS_URL_QUEUE, env),
+      message: REDIS_URL_MESSAGE && expand(REDIS_URL_MESSAGE, env),
+    },
+  );
 
   return {
     cache: REDIS_URL_CACHE && expand(REDIS_URL_CACHE, env),
