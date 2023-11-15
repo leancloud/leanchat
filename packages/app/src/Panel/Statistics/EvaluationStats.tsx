6a931d5a0678e38c993b786ed8da71be2534583a
sdjdd <i@sdjdd.com>
2023-11-15 11:28:33 +0800

    refactor(app): prefer useGetOperatorName

--- packages/app/src/Panel/Statistics/EvaluationStats.tsx
+++ packages/app/src/Panel/Statistics/EvaluationStats.tsx
@@ -5,65 +5,14 @@ import { ColumnsType } from 'antd/es/table';
 import dayjs from 'dayjs';
 
 import { BasicFilterForm, BasicFilterFormData } from './components/BasicFilterForm';
-import { OperatorName, OperatorNameProvider } from './components/OperatorName';
 import {
   EvaluationStats as EvaluationStatsSchema,
   GetEvaluationStatsOptions,
   getEvaluationStats,
 } from '../api/statistics';
 import { getEvaluationStarText } from './utils';
-
-const columns: ColumnsType<EvaluationStatsSchema> = [
-  {
-    dataIndex: 'id',
-    title: '会话ID',
-  },
-  {
-    dataIndex: ['evaluation', 'createdAt'],
-    title: '评价时间',
-    render: (dateString: string) => dayjs(dateString).format('YYYY-MM-DD HH:mm:ss'),
-  },
-  {
-    key: 'evaluationType',
-    title: '评价类型',
-    render: (stats: EvaluationStatsSchema) => {
-      if (stats.evaluationInvitedAt) {
-        return '邀请评价';
-      } else {
-        return '主动评价';
-      }
-    },
-  },
-  {
-    key: 'operatorName',
-    title: '客服名称',
-    render: (stats: EvaluationStatsSchema) => {
-      if (!stats.operatorId) {
-        return '-';
-      }
-      return <OperatorName operatorId={stats.operatorId} />;
-    },
-  },
-  {
-    dataIndex: 'visitorName',
-    title: '用户名称',
-    render: (name: string | undefined) => name ?? '-',
-  },
-  {
-    dataIndex: 'visitorId',
-    title: '用户ID',
-  },
-  {
-    dataIndex: ['evaluation', 'star'],
-    title: '满意度',
-    render: getEvaluationStarText,
-  },
-  {
-    dataIndex: ['evaluation', 'feedback'],
-    title: '评价标签',
-    render: (feedback: string) => feedback || '-',
-  },
-];
+import { useOperators } from '../hooks/operator';
+import { useGetOperatorName } from '../Quality';
 
 export function EvaluationStats() {
   const [formData, setFormData] = useState<BasicFilterFormData>({
@@ -90,8 +39,63 @@ export function EvaluationStats() {
     queryFn: () => getEvaluationStats(options),
   });
 
+  const { data: operators } = useOperators();
+  const getOperatorName = useGetOperatorName(operators);
+
+  const columns: ColumnsType<EvaluationStatsSchema> = [
+    {
+      dataIndex: 'id',
+      title: '会话ID',
+    },
+    {
+      dataIndex: ['evaluation', 'createdAt'],
+      title: '评价时间',
+      render: (dateString: string) => dayjs(dateString).format('YYYY-MM-DD HH:mm:ss'),
+    },
+    {
+      key: 'evaluationType',
+      title: '评价类型',
+      render: (stats: EvaluationStatsSchema) => {
+        if (stats.evaluationInvitedAt) {
+          return '邀请评价';
+        } else {
+          return '主动评价';
+        }
+      },
+    },
+    {
+      key: 'operatorName',
+      title: '客服名称',
+      render: (stats: EvaluationStatsSchema) => {
+        if (!stats.operatorId) {
+          return '-';
+        }
+        return getOperatorName(stats.operatorId);
+      },
+    },
+    {
+      dataIndex: 'visitorName',
+      title: '用户名称',
+      render: (name: string | undefined) => name ?? '-',
+    },
+    {
+      dataIndex: 'visitorId',
+      title: '用户ID',
+    },
+    {
+      dataIndex: ['evaluation', 'star'],
+      title: '满意度',
+      render: getEvaluationStarText,
+    },
+    {
+      dataIndex: ['evaluation', 'feedback'],
+      title: '评价标签',
+      render: (feedback: string) => feedback || '-',
+    },
+  ];
+
   return (
-    <OperatorNameProvider>
+    <>
       <BasicFilterForm initData={formData} onChange={setFormData} />
 
       <Table
@@ -112,6 +116,6 @@ export function EvaluationStats() {
         scroll={{ x: 'max-content' }}
         columns={columns}
       />
-    </OperatorNameProvider>
+    </>
   );
 }
--- packages/app/src/Panel/Statistics/OperatorStats.tsx
+++ packages/app/src/Panel/Statistics/OperatorStats.tsx
@@ -20,200 +20,8 @@ import {
   subtract,
   timeDuration,
 } from './utils';
-import { OperatorName, OperatorNameProvider } from './components/OperatorName';
-
-const columns: ColumnsType<OperatorStatsSchema> = [
-  {
-    dataIndex: 'id',
-    title: '客服ID',
-  },
-  {
-    key: 'operatorName',
-    dataIndex: 'id',
-    title: '客服名称',
-    render: (id) => <OperatorName operatorId={id} />,
-  },
-  {
-    dataIndex: ['conversation', 'totalCount'],
-    title: '咨询总量',
-    render: (value) => value ?? 0,
-  },
-  {
-    dataIndex: ['conversation', 'validCount'],
-    title: '有效会话量',
-    render: (value) => value ?? 0,
-  },
-  {
-    dataIndex: ['conversation', 'invalidCount'],
-    title: '无效会话量',
-    render: (value) => value ?? 0,
-  },
-  {
-    dataIndex: ['conversation', 'operatorNoResponseCount'],
-    title: '客服无应答量',
-    render: (value) => value ?? 0,
-  },
-  {
-    key: 'transferIn',
-    title: '人工转入会话',
-    render: flow(push('transferIn.count'), defaultValue(0)),
-  },
-  {
-    key: 'transferOut',
-    title: '人工转出会话',
-    render: flow(push('transferOut.count'), defaultValue(0)),
-  },
-  {
-    key: 'averageFirstResponseTime',
-    title: '平均首次响应时长',
-    render: flow(push('conversation.averageFirstResponseTime'), timeDuration()),
-  },
-  {
-    key: 'averageResponseTime',
-    title: '平均响应时长',
-    render: flow(
-      push('conversation.responseTime'),
-      push('conversation.responseCount'),
-      divide(),
-      timeDuration(),
-    ),
-  },
-  {
-    key: 'maxResponseTime',
-    title: '最长响应时间',
-    render: flow(push('conversation.maxResponseTime'), timeDuration()),
-  },
-  {
-    key: 'averageValidDuration',
-    title: '平均会话时长',
-    render: flow(
-      push('conversation.validDuration'),
-      push('conversation.validCount'),
-      divide(),
-      timeDuration(),
-    ),
-  },
-  {
-    key: 'averageMessageCount',
-    title: '平均消息条数',
-    render: flow(
-      push('conversation.messageCount'),
-      push('conversation.totalCount'),
-      divide(),
-      fixed(2),
-    ),
-  },
-  {
-    key: 'onlineTime',
-    title: '登录时长',
-    render: flow(push('online.totalTime'), pushValue(1000 * 60), multiply(), timeDuration()),
-  },
-  {
-    key: 'readyTime',
-    title: '在线时长',
-    render: flow(push('online.readyTime'), pushValue(1000 * 60), multiply(), timeDuration()),
-  },
-  {
-    key: 'busyTime',
-    title: '忙碌时长',
-    render: flow(push('online.busyTime'), pushValue(1000 * 60), multiply(), timeDuration()),
-  },
-  {
-    key: 'postprocessingDuration',
-    title: '后处理总时长',
-    render: flow(push('postprocessing.duration'), timeDuration()),
-  },
-  {
-    key: 'postprocessingCount',
-    title: '后处理次数',
-    render: flow(push('postprocessing.count')),
-  },
-  {
-    key: 'leaveTime',
-    title: '离开时长',
-    render: flow(push('online.leaveTime'), pushValue(1000 * 60), multiply(), timeDuration()),
-  },
-  {
-    key: 'averageValidReceptionTime',
-    title: '平均人工接待时长',
-    render: flow(
-      push('conversation.validReceptionTime'),
-      push('conversation.validCount'),
-      divide(),
-      timeDuration(),
-    ),
-  },
-  {
-    key: 'totalEvaluationCount',
-    title: '评价总数',
-    render: flow(push('conversation.validEvaluationCount')),
-  },
-  {
-    key: 'noEvaluationCount',
-    title: '未评价总数',
-    render: flow(
-      push('conversation.validCount'),
-      push('conversation.validEvaluationCount'),
-      subtract(),
-    ),
-  },
-  {
-    key: 'evaluationPercentage',
-    title: '参评率',
-    render: flow(
-      push('conversation.validEvaluationCount'),
-      push('conversation.validCount'),
-      divide(),
-      percent(),
-    ),
-  },
-  {
-    dataIndex: ['conversation', 'validEvaluationInvitationCount'],
-    title: '客服邀请量',
-  },
-  {
-    key: 'evaluationInvitationPercentage',
-    title: '客服邀请率',
-    render: flow(
-      push('conversation.validEvaluationInvitationCount'),
-      push('conversation.validCount'),
-      divide(),
-      percent(),
-    ),
-  },
-  {
-    key: 'negativeRate',
-    title: '差评率',
-    render: flow(
-      push('conversation.evaluationStar1'),
-      push('conversation.evaluationStar2'),
-      add(),
-      push('conversation.validEvaluationCount'),
-      divide(),
-      percent(),
-    ),
-  },
-  {
-    dataIndex: ['conversation', 'evaluationStar5'],
-    title: '非常满意量',
-  },
-  {
-    dataIndex: ['conversation', 'evaluationStar4'],
-    title: '满意量',
-  },
-  {
-    dataIndex: ['conversation', 'evaluationStar3'],
-    title: '一般量',
-  },
-  {
-    dataIndex: ['conversation', 'evaluationStar2'],
-    title: '不满意量',
-  },
-  {
-    dataIndex: ['conversation', 'evaluationStar1'],
-    title: '非常不满意量',
-  },
-];
+import { useGetOperatorName } from '../Quality';
+import { useOperators } from '../hooks/operator';
 
 export function OperatorStats() {
   const [formData, setFormData] = useState<BasicFilterFormData>({
@@ -235,8 +43,204 @@ export function OperatorStats() {
     queryFn: () => getOperatorStats(options),
   });
 
+  const { data: operators } = useOperators();
+  const getOperatorName = useGetOperatorName(operators);
+
+  const columns: ColumnsType<OperatorStatsSchema> = [
+    {
+      dataIndex: 'id',
+      title: '客服ID',
+    },
+    {
+      key: 'operatorName',
+      dataIndex: 'id',
+      title: '客服名称',
+      render: getOperatorName,
+    },
+    {
+      dataIndex: ['conversation', 'totalCount'],
+      title: '咨询总量',
+      render: (value) => value ?? 0,
+    },
+    {
+      dataIndex: ['conversation', 'validCount'],
+      title: '有效会话量',
+      render: (value) => value ?? 0,
+    },
+    {
+      dataIndex: ['conversation', 'invalidCount'],
+      title: '无效会话量',
+      render: (value) => value ?? 0,
+    },
+    {
+      dataIndex: ['conversation', 'operatorNoResponseCount'],
+      title: '客服无应答量',
+      render: (value) => value ?? 0,
+    },
+    {
+      key: 'transferIn',
+      title: '人工转入会话',
+      render: flow(push('transferIn.count'), defaultValue(0)),
+    },
+    {
+      key: 'transferOut',
+      title: '人工转出会话',
+      render: flow(push('transferOut.count'), defaultValue(0)),
+    },
+    {
+      key: 'averageFirstResponseTime',
+      title: '平均首次响应时长',
+      render: flow(push('conversation.averageFirstResponseTime'), timeDuration()),
+    },
+    {
+      key: 'averageResponseTime',
+      title: '平均响应时长',
+      render: flow(
+        push('conversation.responseTime'),
+        push('conversation.responseCount'),
+        divide(),
+        timeDuration(),
+      ),
+    },
+    {
+      key: 'maxResponseTime',
+      title: '最长响应时间',
+      render: flow(push('conversation.maxResponseTime'), timeDuration()),
+    },
+    {
+      key: 'averageValidDuration',
+      title: '平均会话时长',
+      render: flow(
+        push('conversation.validDuration'),
+        push('conversation.validCount'),
+        divide(),
+        timeDuration(),
+      ),
+    },
+    {
+      key: 'averageMessageCount',
+      title: '平均消息条数',
+      render: flow(
+        push('conversation.messageCount'),
+        push('conversation.totalCount'),
+        divide(),
+        fixed(2),
+      ),
+    },
+    {
+      key: 'onlineTime',
+      title: '登录时长',
+      render: flow(push('online.totalTime'), pushValue(1000 * 60), multiply(), timeDuration()),
+    },
+    {
+      key: 'readyTime',
+      title: '在线时长',
+      render: flow(push('online.readyTime'), pushValue(1000 * 60), multiply(), timeDuration()),
+    },
+    {
+      key: 'busyTime',
+      title: '忙碌时长',
+      render: flow(push('online.busyTime'), pushValue(1000 * 60), multiply(), timeDuration()),
+    },
+    {
+      key: 'postprocessingDuration',
+      title: '后处理总时长',
+      render: flow(push('postprocessing.duration'), timeDuration()),
+    },
+    {
+      key: 'postprocessingCount',
+      title: '后处理次数',
+      render: flow(push('postprocessing.count')),
+    },
+    {
+      key: 'leaveTime',
+      title: '离开时长',
+      render: flow(push('online.leaveTime'), pushValue(1000 * 60), multiply(), timeDuration()),
+    },
+    {
+      key: 'averageValidReceptionTime',
+      title: '平均人工接待时长',
+      render: flow(
+        push('conversation.validReceptionTime'),
+        push('conversation.validCount'),
+        divide(),
+        timeDuration(),
+      ),
+    },
+    {
+      key: 'totalEvaluationCount',
+      title: '评价总数',
+      render: flow(push('conversation.validEvaluationCount')),
+    },
+    {
+      key: 'noEvaluationCount',
+      title: '未评价总数',
+      render: flow(
+        push('conversation.validCount'),
+        push('conversation.validEvaluationCount'),
+        subtract(),
+      ),
+    },
+    {
+      key: 'evaluationPercentage',
+      title: '参评率',
+      render: flow(
+        push('conversation.validEvaluationCount'),
+        push('conversation.validCount'),
+        divide(),
+        percent(),
+      ),
+    },
+    {
+      dataIndex: ['conversation', 'validEvaluationInvitationCount'],
+      title: '客服邀请量',
+    },
+    {
+      key: 'evaluationInvitationPercentage',
+      title: '客服邀请率',
+      render: flow(
+        push('conversation.validEvaluationInvitationCount'),
+        push('conversation.validCount'),
+        divide(),
+        percent(),
+      ),
+    },
+    {
+      key: 'negativeRate',
+      title: '差评率',
+      render: flow(
+        push('conversation.evaluationStar1'),
+        push('conversation.evaluationStar2'),
+        add(),
+        push('conversation.validEvaluationCount'),
+        divide(),
+        percent(),
+      ),
+    },
+    {
+      dataIndex: ['conversation', 'evaluationStar5'],
+      title: '非常满意量',
+    },
+    {
+      dataIndex: ['conversation', 'evaluationStar4'],
+      title: '满意量',
+    },
+    {
+      dataIndex: ['conversation', 'evaluationStar3'],
+      title: '一般量',
+    },
+    {
+      dataIndex: ['conversation', 'evaluationStar2'],
+      title: '不满意量',
+    },
+    {
+      dataIndex: ['conversation', 'evaluationStar1'],
+      title: '非常不满意量',
+    },
+  ];
+
   return (
-    <OperatorNameProvider>
+    <>
       <BasicFilterForm initData={formData} onChange={setFormData} />
 
       <Table
@@ -247,6 +251,6 @@ export function OperatorStats() {
         scroll={{ x: 'max-content' }}
         columns={columns}
       />
-    </OperatorNameProvider>
+    </>
   );
 }
