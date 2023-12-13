533ade1623b76362d85f06b6f0240c8f7a933f23
sdjdd <i@sdjdd.com>
2023-12-13 17:18:26 +0800

    feat(server): trim evaluation data

--- packages/server/src/visitor-channel/dtos/evaluation.dto.ts
+++ packages/server/src/visitor-channel/dtos/evaluation.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 const EvaluationSchema = z.object({
   star: z.number().int().min(1).max(5),
-  feedback: z.string().optional(),
+  feedback: z.string().trim().optional(),
   tags: z.array(z.string()).optional(),
 });
 
